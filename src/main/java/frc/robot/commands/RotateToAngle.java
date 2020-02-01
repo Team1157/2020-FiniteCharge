4e50e013de75a4e2221a7d6368c358cb8570490f
Noah <noah@mollerstuen.com>
2020-02-01 12:34:26 -0700

    Added climber subsystem template

--- src/main/java/frc/robot/commands/RotateToAngle.java
+++ src/main/java/frc/robot/commands/RotateToAngle.java
@@ -9,7 +9,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
-//import frc.robot.subsystems.ExampleSubsystem;
 
 import java.util.function.DoubleSupplier;
 
